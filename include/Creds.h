083be7d0ce708751658cb701c20b104637cf3188
DevJoseWeb <web2ajax@gmail.com>
2019-03-27 21:16:37 -0300

    firebase define

--- include/Creds.h
+++ include/Creds.h
@@ -1,5 +1,12 @@
 #define SSID "PLANET_D9CM"
 #define PASSWORD "FVMSJ4EZ"
 
-#define FIREBASE_HOST "medium-iot-project.firebaseio.com"
-#define FIREBASE_AUTH "lr6Wp38WJNod6dOmVsbBPstVfPzf8QpObXf8fJIv"
+#define FIREBASE_HOST "https://hemoce-iot.firebaseio.com"
+#define FIREBASE_AUTH "AIzaSyAN36l7pR-CDmUMCx0Zk9z92Shl37bvah0"
+
+#define apiKey "AIzaSyAN36l7pR-CDmUMCx0Zk9z92Shl37bvah0",
+#define authDomain "hemoce-iot.firebaseapp.com",
+#define databaseURL "https://hemoce-iot.firebaseio.com",
+#define projectId "hemoce-iot",
+#define storageBucket "hemoce-iot.appspot.com",
+#define messagingSenderId "638554938479"
